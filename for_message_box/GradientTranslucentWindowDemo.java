b9403744d26f82ef4e90d8f9ccf6195e43f156f1
mahesh3097 <37842788+mahesh3097@users.noreply.github.com>
2018-03-29 13:24:42 +0530

    host string coming on screen

--- for_message_box/GradientTranslucentWindowDemo.java
+++ for_message_box/GradientTranslucentWindowDemo.java
@@ -1,87 +1,114 @@
-import java.awt.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
 import javax.swing.*;
-//import javax.util.Timer;
+import java.awt.*;
 import static java.awt.GraphicsDevice.WindowTranslucency.*;
 
-public class GradientTranslucentWindowDemo extends JFrame {
-    public GradientTranslucentWindowDemo() {
-        super("GradientTranslucentWindow");
 
-        setBackground(new Color(0,0,0,0));
-        setSize(new Dimension(320, 240));				//modify the size
-        setLocation(60,120);					// set location to bottom
+public class host_string {
 
-        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // replace with timer 
-		
-		setUndecorated(true);
 
-        JPanel panel = new JPanel() {
-            @Override
-            protected void paintComponent(Graphics g) {
-                if (g instanceof Graphics2D) {
-                    final int R = 250;
-                    final int G = 250;
-                    final int B = 250;
 
-                    Paint p =
-                        new GradientPaint(0.0f, 0.0f, new Color(R, G, B, 0),
-                            0.0f, getHeight(), new Color(R, G, B, 255), true);
-                    Graphics2D g2d = (Graphics2D)g;
-                    g2d.setPaint(p);
-                    g2d.fillRect(0, 0, getWidth(), getHeight());
-                }
-            }
-        };
-        setContentPane(panel);
-        setLayout(new GridBagLayout());
-		JLabel label1 = new JLabel("Test");
-        add(label1);
-		
-		
-    }
+
+
+
 
     public static void main(String[] args) {
-        // Determine what the GraphicsDevice can support.
-        GraphicsEnvironment ge = 
-            GraphicsEnvironment.getLocalGraphicsEnvironment();
-        GraphicsDevice gd = ge.getDefaultScreenDevice();
-        boolean isPerPixelTranslucencySupported = 
-            gd.isWindowTranslucencySupported(PERPIXEL_TRANSLUCENT);
 
-        //If translucent windows aren't supported, exit.
-        if (!isPerPixelTranslucencySupported) {
-            System.out.println(
-                "Per-pixel translucency is not supported");
-                System.exit(0);
-        }
+JFrame frame =new JFrame();
+frame.setSize(600,400);
+frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-        JFrame.setDefaultLookAndFeelDecorated(true);
+JPanel panel=new JPanel() ;
+panel.setBackground(Color.YELLOW);
+//frame.setVisible(true);
 
-        // Create the GUI on the event-dispatching thread
-        SwingUtilities.invokeLater(new Runnable() {
-            @Override
-			//Timer timer = new Timer();
-			//TimerTask task = new TimerTask(){	
-            public void run() 
-			{
-                GradientTranslucentWindowDemo gtw = new
-                    GradientTranslucentWindowDemo();
 
-                // Display the window.
-                gtw.setVisible(true);
-				try{Thread.sleep(3000);}
-				catch(Exception e){
-					//System.println("message");
-				}
-				
-			
-				gtw.dispose();
-					
-				//setTimeout(function(){ gtw.dispose() }, 1000);
-				
 
-				
+
+        System.out.println("Server start");
+        System.out.println("Runtime Java: " 
+                + System.getProperty("Java.runtime.version"));
+        
+        ServerSocket serverSocket = null;
+        Socket clientSocket = null;
+        BufferedReader bufferedReader = null;
+        PrintWriter printWriter = null;
+        
+        try {
+            serverSocket = new ServerSocket(8090);
+            System.out.println("Server port: " 
+                    + serverSocket.getLocalPort());
+            
+            clientSocket = serverSocket.accept();
+            
+            //Client connected
+
+            InputStream inputStream = clientSocket.getInputStream();
+            InputStreamReader inputStreamReader = new InputStreamReader(inputStream);
+            bufferedReader = new BufferedReader(inputStreamReader);
+            
+            OutputStream outputStream = clientSocket.getOutputStream();
+            printWriter = new PrintWriter(outputStream, true);
+
+            String line;
+
+            while((line = bufferedReader.readLine()) != null){
+                System.out.println(line);
+                JLabel label = new JLabel(line);
+               
+				panel.add(label);
+				frame.add(panel);
+				frame.setVisible(true);
+
+                printWriter.println(line);  //echo back to sender
+            };
+            
+        
+} catch (IOException ex) {
+            System.err.println(ex.toString());
+        }finally{
+            
+            if(printWriter != null){
+                printWriter.close();
+                System.out.println("printWriter closed");
             }
-        });
+            
+            if(bufferedReader != null){
+                try {
+                    bufferedReader.close();
+                    System.out.println("bufferedReader closed");
+                } catch (IOException ex) {
+                    System.out.println(ex.toString());
+                }
+            }
+            
+            if(clientSocket != null){
+                try {
+                    clientSocket.close();
+                    System.out.println("clientSocket closed");
+                } catch (IOException ex) {
+                    System.out.println(ex.toString());
+                }
+            }
+            
+            if(serverSocket != null){
+                try {
+                    serverSocket.close();
+                    System.out.println("serverSocket closed");
+                } catch (IOException ex) {
+                    System.out.println(ex.toString());
+                }
+            }
+            
+        }
+        
     }
+    
 }
